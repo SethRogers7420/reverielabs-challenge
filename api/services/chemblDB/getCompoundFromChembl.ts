e60feaa4f77dcd99db54296cbc09049c19e78b2f
Seth Rogers <Seth.Rogers7420@gmail.com>
2023-01-29 17:58:07 -0500

    Include data for charts.

--- api/services/chemblDB/getCompoundFromChembl.ts
+++ api/services/chemblDB/getCompoundFromChembl.ts
@@ -5,6 +5,7 @@ import * as mathjs from "mathjs";
 import { Cache } from "../memoryCache/cacheTypes";
 
 export type ChemblInfo = {
+  moleculeInfo: MoleculeInfo[];
   ic50Aggregate: {
     mean: number;
     median: number;
@@ -13,7 +14,13 @@ export type ChemblInfo = {
   selectedSmiles: SmileInfo[];
 };
 
-type SmileInfo = { molecule_chembl_id: string; smiles: string };
+export type MoleculeInfo = {
+  molecule_chembl_id: string;
+  pchembl_value: number | null;
+  full_mwt: number | null;
+};
+
+export type SmileInfo = { molecule_chembl_id: string; smiles: string };
 
 export async function getCompoundFromChembl(
   chemblID: string,
@@ -35,7 +42,10 @@ export async function getCompoundFromChembl(
   // Get all the valid IC50 numbers, removing strings or nulls.
   const ic50numbers = getIC50Values(chemblRows);
 
+  const moleculeInfo = getMoleculeInfo(chemblRows);
+
   const chemblInfo: ChemblInfo = {
+    moleculeInfo,
     ic50Aggregate:
       ic50numbers.length > 0
         ? {
@@ -83,17 +93,12 @@ function getIC50Values(chemblRows: ChemblRow[]): number[] {
   const ic50numbers: number[] = [];
 
   for (const chemblRow of chemblRows) {
-    const ic50string = chemblRow._source.pchembl_value;
-    if (ic50string == null || isEmpty(ic50string)) {
+    const ic50 = stringToFloat(chemblRow._source.pchembl_value);
+    if (ic50 == null) {
       continue;
     }
 
-    const ic50ParsedNumber = parseFloat(ic50string);
-    if (isNaN(ic50ParsedNumber)) {
-      continue;
-    }
-
-    ic50numbers.push(ic50ParsedNumber);
+    ic50numbers.push(ic50);
   }
 
   return ic50numbers;
@@ -128,4 +133,25 @@ function getSelectedSmiles(chemblRows: ChemblRow[]): SmileInfo[] {
   }
 
   return smiles;
+}
+
+function getMoleculeInfo(chemblRows: ChemblRow[]): MoleculeInfo[] {
+  return chemblRows.map<MoleculeInfo>((chemblRow) => {
+    return {
+      molecule_chembl_id: chemblRow._source.molecule_chembl_id,
+      full_mwt: stringToFloat(
+        chemblRow._source._metadata?.parent_molecule_data?.full_mwt
+      ),
+      pchembl_value: stringToFloat(chemblRow._source.pchembl_value)
+    };
+  });
+}
+
+function stringToFloat(str: string | null | undefined): number | null {
+  let float = str != null ? parseFloat(str) : null;
+  if (float != null && isNaN(float)) {
+    float = null;
+  }
+
+  return float;
 }
--- client/src/api/getChemblData.ts
+++ client/src/api/getChemblData.ts
@@ -1,13 +1,20 @@
 import { makeGetRequest } from "./makeGetRequest";
 
-// This type should normally come from something like a Swagger file or a schema.graphql file.
+// This type should normally come from something like a Swagger file or a schema.graphql file if using GraphQL
 export type ChemblInfo = {
+  moleculeInfo: MoleculeInfo[];
   ic50Aggregate: {
     mean: number;
     median: number;
     standardDeviation: number;
   } | null;
   selectedSmiles: SmileInfo[];
+};
+
+export type MoleculeInfo = {
+  molecule_chembl_id: string;
+  pchembl_value: number | null;
+  full_mwt: number | null;
 };
 
 export type SmileInfo = { molecule_chembl_id: string; smiles: string };
